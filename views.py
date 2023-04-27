0ec0f14e90c8428df122b16dc74701f9e42ee8ad
Jaewoo Lee Private <jaewoo.lee90@gmail.com>
2023-04-27 16:26:38 -0400

    minor fix commit

--- views.py
+++ views.py
@@ -1,25 +1,3 @@
-# from flask import Blueprint, render_template, request, flash, session
-
-
-# views = Blueprint(__name__, "views")
-
-
-# @views.route("/")
-# def index():
-#     if 'name' in session:
-#         flash('Hi, ' + session['name'] + ', Welcome back!')
-#         return render_template("index.html")
-#     else:
-#         flash("Tell me your name!")
-#         return render_template("index.html")
-
-# @views.route("/greet", methods=["POST", "GET"])
-# def greet():
-#     name = str(request.form['name']).capitalize()
-#     flash("Hi " + str(request.form['name']).capitalize()+ ", great to see you!")
-#     session['name'] = name
-#     return render_template("index.html")
-
 from flask import Blueprint, render_template, request, flash, make_response
 
 views = Blueprint(__name__, "views")
